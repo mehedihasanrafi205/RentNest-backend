a9c4d25df9d2998a392869d41f9512787788409e
MehediHasanRafi00 <mehedihasanrafi205@gmail.com>
2026-07-06 18:36:59 +0600

    fix: correct auth middleware import in category route

--- src/models/category/category.route.ts
+++ src/models/category/category.route.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { CategoryControllers } from './category.controller';
-import auth from '../../middlewares/auth';
+import { auth } from '../../middlewares/auth';
 import { UserRole } from '../../generated/prisma/client';
 
 const router = express.Router();
